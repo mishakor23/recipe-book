34c937bcdc4fb01a12583e93247ea71c1e9d7a37
Mikhail Korotkikh <mishakor23@gmail.com>
2016-09-19 18:22:58 +0300

    add looping throught the ingredients in shopping llist component

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../shared';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html'
 })
 export class ShoppingListComponent implements OnInit {
+  items: Ingredient[] = [];
 
   constructor() { }
 
