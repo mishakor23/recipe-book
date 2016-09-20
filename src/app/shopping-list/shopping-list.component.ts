c54acbf99e2f42b50ea9f9bea5db7a68628f9e20
Mikhail Korotkikh <mishakor23@gmail.com>
2016-09-20 15:04:44 +0300

    add ingredients displaying

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Ingredient } from '../shared';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list',
@@ -9,9 +10,10 @@ import { Ingredient } from '../shared';
 export class ShoppingListComponent implements OnInit {
   items: Ingredient[] = [];
 
-  constructor() { }
+  constructor(private sls: ShoppingListService) { }
 
   ngOnInit() {
+    this.items = this.sls.getItems();
   }
 
 }
