c4f6b77b1c87cbcabf3efc5bab4d6f67c1fcc270
Mikhail Korotkikh <mishakor23@gmail.com>
2016-09-21 13:22:06 +0300

    add cildren routes to the project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { recipes_routes } from './recipes/recipes.routes';
 
 const app_routes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  { path: 'recipes', component: RecipesComponent },
+  { path: 'recipes', component: RecipesComponent, children: recipes_routes },
   { path: 'shopping-list', component: ShoppingListComponent }
 ];
 
--- src/app/recipes/recipes.routes.ts
+++ src/app/recipes/recipes.routes.ts
@@ -4,11 +4,11 @@ import { RecipeStartComponent } from './recipe-start.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
 
-const recipes_routes: Routes = [
+export const recipes_routes: Routes = [
   { path: '', component: RecipeStartComponent },
   { path: 'new', component: RecipeEditComponent },
   { path: ':id', component: RecipeDetailComponent },
   { path: ':id/edit', component: RecipeEditComponent },
 ];
 
-export const recipes_routing = RouterModule.forRoot(recipes_routes);
+// export const recipes_routing = RouterModule.forRoot(recipes_routes);
