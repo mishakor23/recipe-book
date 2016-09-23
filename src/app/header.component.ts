17ff472435db50bd40cf5c64676b6faf674fcd7b
Mikhail Korotkikh <mishakor23@gmail.com>
2016-09-23 15:43:43 +0300

    add posibility to store and fetch data from firebase

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -1,14 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { RecipeService } from './recipes/recipe.service';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
-  ngOnInit() {
+  onStore() {
+    this.recipeService.storeData()
+      .subscribe(
+        data => console.log(data)
+      );
+  }
+
+  onFetch() {
+    this.recipeService.fetchData();
   }
 
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,10 +1,14 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
+import { Headers, Http, Response } from '@angular/http';
+import 'rxjs/Rx';
 
 import { Recipe } from './recipe';
 import { Ingredient } from '../shared/ingredient';
 
 @Injectable()
 export class RecipeService {
+  recipesChanged = new EventEmitter<Recipe[]>();
+
   private recipes: Recipe[] = [
     new Recipe('Banana Bread', 'This banana bread recipe is moist and delicious, with loads of banana flavor. It\'s wonderful toasted!', 'http://images.media-allrecipes.com/userphotos/250x250/914962.jpg', [
       new Ingredient('Banana', 3),
@@ -14,7 +18,7 @@ export class RecipeService {
     new Recipe('Italian BBQ Pork Chops', `Pork chops with a quick homemade BBQ sauce made with balsamic vinegar, ketchup, and brown sugar have just the right amount of sweetness to become addictive.`, 'http://images.media-allrecipes.com/userphotos/250x250/659267.jpg', [])
   ];
 
-  constructor() { }
+  constructor(private http: Http) { }
 
   getRecipes() {
     return this.recipes;
@@ -26,5 +30,32 @@ export class RecipeService {
 
   deleteRecipe(recipe: Recipe) {
     this.recipes.splice(this.recipes.indexOf(recipe), 1);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+  }
+
+  editRecipe(oldRecipe: Recipe, newRecipe: Recipe) {
+    this.recipes[this.recipes.indexOf(oldRecipe)] = newRecipe;
+  }
+
+  storeData() {
+    const body = JSON.stringify(this.recipes);
+    const headers = new Headers({
+      'Content-Type': 'application/json'
+    });
+    return this.http.put('https://recipe-book-22592.firebaseio.com/recipes.json', body, {headers: headers});
+  }
+
+  fetchData() {
+    return this.http.get('https://recipe-book-22592.firebaseio.com/recipes.json')
+      .map((response: Response) => response.json())
+      .subscribe(
+        (data: Recipe[]) => {
+          this.recipes = data;
+          this.recipesChanged.emit(this.recipes);
+        }
+      );
   }
 }
